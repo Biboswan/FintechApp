4d262397e1a1400eaececeb5699847176a817589
Biboswan Roy <biboswan98@gmail.com>
2017-11-04 22:01:08 +0530

    Added line chart

--- app/src/server.js
+++ app/src/server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
+var engines = require('consolidate');
+var path=require('path');
 
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
 //your routes here
 app.get('/', function (req, res) {
-    res.send("Hello World!");
+    res.render('index.html');
 });
+
+app.get('/utils.js',function(req,res){
+	res.sendFile(path.join(__dirname,'public','utils.js'))
+})
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
